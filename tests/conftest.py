30aae3ff69fdad4c7e88f22a1185a8ad0e6297e9
lunzhou <lunzhou@yunify.com>
2023-12-15 23:10:39 +0800

    chore: chart & cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,7 @@ import pytest
 from requests_toolbelt.sessions import BaseUrlSession
 
 
-env = os.getenv('ENV')
-envs = {
-    'loc': 'http://127.0.0.1:8001'
-}
-
-
 @pytest.fixture()
 def init(request):
-    url = envs[env]
-    bs = BaseUrlSession(base_url=url)
+    bs = BaseUrlSession(base_url='http://127.0.0.1:8001')
     request.cls.bs = bs
